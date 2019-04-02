b7fbc2d15c96fe5d2fd32d1e54072716b6db3b10
Marc Wright <marc@generalassemb.ly>
2019-04-02 14:48:24 +0300

    push ajax starter

---
+++ starter-code/promises-exercise/main.js
@@ -0,0 +1,21 @@
+'use strict';
+(function() {
+  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather/?units=metric";
+  var userInput = 'Riyadh';
+  var appid = '1b200fe2ac60cb6acd29571cf2f0b34d';
+
+  $.ajax({
+    url: weatherUrl,
+    type: 'GET',
+    data: {
+      q: userInput,
+      appid: appid
+    },
+    // success: function(response){
+    //   console.log(response)
+    // }
+  })
+  .then(function(response) {
+    console.log(response)
+  })
+})()
